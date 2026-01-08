38d26678a481e5ec466244708814c4b9d04fc912
ZikoBkr06 <zakariaboukria2017@gmail.com>
2026-01-08 22:35:25 +0100

    ajout de menu_router.py final

--- POJECT CMS/controllers/menu_router.py
+++ POJECT CMS/controllers/menu_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from database import SessionLocal
 from services.menu_service import MenuService
-from dto.menu_dto import MenuItemCreate, MenuItemOut
+from dto.menu_dto import MenuItemCreate, MenuItemOut, MenuUpdateDTO
 # from models.menu_model import MenuItem
 menu_router = APIRouter(prefix="/menu", tags=["Menu"])
 
@@ -25,9 +25,20 @@ def add_item(item: MenuItemCreate, db: Session = Depends(get_db)):
     return MenuService(db).add_item(item)
 
 
+@menu_router.put("/{item_id}", response_model=MenuItemOut)
+def update_item(item_id: int, item: MenuUpdateDTO, db: Session = Depends(get_db)):
+    updated = MenuService(db).update_item(item_id, item)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return updated
+
+
 @menu_router.delete("/{item_id}")
 def delete_item(item_id: int, db: Session = Depends(get_db)):
     deleted = MenuService(db).delete_item(item_id)
     if deleted:
         return {"message": "Item deleted"}
     raise HTTPException(status_code=404, detail="Item not found")
+
+
+
