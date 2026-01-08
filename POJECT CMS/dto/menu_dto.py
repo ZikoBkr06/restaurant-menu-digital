ab2952e74f5e13173a3e5060088e3019a39908ff
ZikoBkr06 <zakariaboukria2017@gmail.com>
2026-01-08 22:30:04 +0100

    ajout de menu_dto.py final

--- POJECT CMS/dto/menu_dto.py
+++ POJECT CMS/dto/menu_dto.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
+
 # from models.menu_model import MenuItem
 class MenuItemBase(BaseModel):
     name: str
     price: float
-    description: str | None = None
+    description: str | None = None  
     image_url: str | None = None
 
 class MenuItemCreate(MenuItemBase):
@@ -14,3 +16,10 @@ class MenuItemOut(MenuItemBase):
 
     class Config:
         orm_mode = True
+
+
+
+class MenuUpdateDTO(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
