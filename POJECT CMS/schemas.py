505a7432871179f3f6946f31bee34eb8dbcdceab
ZikoBkr06 <zakariaboukria2017@gmail.com>
2026-01-08 16:02:40 +0100

    ajout de schemas.py final

--- POJECT CMS/schemas.py
+++ POJECT CMS/schemas.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel, ConfigDict
 
 # -------- EMPLOYEES --------
-
+# Employee DTOs
 class EmployeeCreate(BaseModel):
     full_name: str
     role: str
     phone: str
     email: str
 
+
+# Employee Read DTO
 class EmployeeRead(EmployeeCreate):
     id: int
 
